d5cd51f17edfb051202a742be4ae604d9d48f8b2
divyaprasanthi <103738516+divyaprasanthi@users.noreply.github.com>
2022-04-17 12:59:31 +0530

    input

---
+++ input.py
@@ -0,0 +1,39 @@
+# num1=input("enter first number:")
+# num2=input("enter second number:")
+# print(num1+num2)
+# num1=int(input("enter first number:"))
+# num2=int(input("enter second number:"))
+# print(num1+num2)
+# num1=input("enter first number:")
+# num2=input("enter second number:")
+# print(num1+ " " +num2)
+# # sting to int
+# num1=input("enter first decimal number:")
+# num2=input("enter second decimal number:")
+# print(num1+num2)
+# # string to float
+# num1=input("enter first decimal number:")
+# num2=input("enter second decimal number:")
+# print(float(num1)+float(num2))
+# num1=input("enter first decimal number:")
+# num2=input("enter second integer number:")
+# print(float(num1)+int(num2))
+# num1=input("enter first decimal number:")
+# num2=input("enter second decimal number:")
+# print(float(num1)+float(num2))
+name="john"
+age=25
+sal=300000.9
+# approach1
+print(name,age,sal)
+# approach 2
+print("name is:",name)
+print("age is:",age)
+print("sal is:",sal)
+# approach3
+print("name:%s age:%d salary:%g"%(name,age,sal))
+# approach4
+print("name: {} age: {} salary {}" .format(name,age,sal))
+# approach5
+print("name:{0} age:{1} saalry:{2}".format(name,age,sal))
+
